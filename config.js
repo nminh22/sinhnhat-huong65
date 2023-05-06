375ebc37a6bde0ba81da9e821752a140b7a1284b
nminh22 <123850411+nminh22@users.noreply.github.com>
2023-05-06 10:12:23 +0700

    Add files via upload
    creat card

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = '';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh nhật chị';
+const titleCard = 'Tặng Hương';
+const contentCard = 'Chúc chị Hương sinh nhật vui vẻ nhó tuổi mới bớt hỗn, xinh đẹp hơn,quan trọng là chúc chị đỗ cấp 3 và luôn giữ vui vẻ nhéeee';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+
+const giftImage = 'https://photos.google.com/search/_tra_/photo/AF1QipNhQx6cvtIqvnVjiI_MRFR3w-RUOV80CiRDryDL';
+const base64 = 'https://photos.google.com/search/_tra_/photo/AF1QipNhQx6cvtIqvnVjiI_MRFR3w-RUOV80CiRDryDL';
+const giftImageBase64 = "data:image/png;base64, " + base64;
